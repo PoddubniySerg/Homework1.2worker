d5af2d2f03dbb0b26a941bf66d48f5bb44d52847
Poddubniy Sergey <poddubniysergey1981@gmail.com>
2022-03-04 23:44:10 +0300

    new files

---
+++ src/Worker.java
@@ -0,0 +1,20 @@
+public class Worker {
+
+    private Main.OnTaskDoneListener callback;
+    private Main.OnTaskErrorListener errorCallback;
+
+    public Worker(Main.OnTaskDoneListener callback, Main.OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " - Error");
+            } else {
+                callback.onDone("Task " + i + " is done");
+            }
+        }
+    }
+}
